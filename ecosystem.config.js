f9a6c068f4458416eeb92794e3938cca1e403cdb
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-07 12:43:14 -0700

    pm2 start file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
+        // First application
+        {
+            name: 'SimplifyMe',
+            script: 'app.js',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+
+    ]
+};
