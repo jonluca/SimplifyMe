2ea9063af8b3e094c6fb177c228a445979cf57ee
JonLuca DeCaro <jdecaro@usc.edu>
2018-01-02 10:59:11 +0100

    suburl prefixing fixed

--- public/js/main.js
+++ public/js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
         } else {
             $.ajax({
                 method: 'POST',
-                url: "/SimplifyMe/search",
+                url: "search",
                 type: 'json',
                 data: {
                     url: url.value
